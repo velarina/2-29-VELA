54093a8967d71c8692dadd889f36f00fc6aca7e5
unknown <velarina2014@gmail.com>
2022-02-06 22:01:12 +0700

    upload modul 2

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,20 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',[
+        "title" => "Home"
+    ]);
 });
 Route::get('/about', function () {
     return view('about',[
+        "title" => "About",
         "nama"=>"Velarina Nurmalakana",
         "email"=>"velarina2014@gmail.com",
         "gambar"=>"vela.jpg"
     ]);
 });
 Route::get('/galery', function () {
-    return view('galery');
+    return view('galery',[
+        "title" => "Gallery"
+    ]);
 });
